856159a0012ee8a8f158c02d6ce1302d6d36bf68
Nasra <nasra@MacBookAir.lan>
2025-07-04 12:31:10 +0200

    ajout exercices

---
+++ script.js
@@ -0,0 +1,21 @@
+const containerQuote = document.getElementById("container-quote")
+const quote = document.getElementById("quote")
+const authorName = document.getElementById("author-name")
+const buttonSubmit = document.getElementById("submit-quote")
+const quoteList = document.getElementById("quote-list")
+const quoteText = document.getElementsByClassName("text")
+const authorQuote = document.getElementsByClassName("author")
+const p = document.createElement("p")
+
+
+
+containerQuote.addEventListener(("submit"),(event) =>{ event.preventDefault()
+const text = quote.value
+const author= authorName.value
+console.log(text, author)
+
+})
+
+function addQuote(quote, author){
+
+}
